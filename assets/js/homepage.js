3ee42f851adcfce405a3e64a55386f1952e78de1
MFG <macfglenn@gmail.com>
2021-11-05 03:23:20 -0400

    added buttons to see featured language issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -24,20 +25,21 @@ var getUserRepos = function (user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a request to the url
-  fetch(apiUrl).then(function (respone) {
-    // request was succesful
-    if (respone.ok) {
-      respone.json().then(function (data) {
-        displayRepos(data, user);
-      });
-    } else {
-      alert("Error: GitHub User Not Found");
-    }
-  })
-  .catch(function(error) {
-    // Notice this '.catch()' getting chained onto the end of the '.then()' method
-    alert("Unable to connect to GitHub");
-  });
+  fetch(apiUrl)
+    .then(function (respone) {
+      // request was succesful
+      if (respone.ok) {
+        respone.json().then(function (data) {
+          displayRepos(data, user);
+        });
+      } else {
+        alert("Error: GitHub User Not Found");
+      }
+    })
+    .catch(function (error) {
+      // Notice this '.catch()' getting chained onto the end of the '.then()' method
+      alert("Unable to connect to GitHub");
+    });
 };
 
 var displayRepos = function (repos, searchTerm) {
@@ -91,4 +93,32 @@ var displayRepos = function (repos, searchTerm) {
   }
 };
 
+var getFeaturedRepos = function (language) {
+  var apiUrl =
+    "https://api.github.com/search/repositories?q=" +
+    language +
+    "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: GitHub User Not Found");
+    }
+  });
+};
+
+var buttonClickHandler = function (event) {
+  var language = event.target.getAttribute("data-language");
+  if (language) {
+    getFeaturedRepos(language);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
